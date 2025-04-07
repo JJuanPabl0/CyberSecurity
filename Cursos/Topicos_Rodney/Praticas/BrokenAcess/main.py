759ffd49089c0388df1f4cc50f43f17fa85ce486
JJuanPabl0 <juansilverio865@gmail.com>
2025-04-07 11:46:18 -0300

    feat: crianção do banco de dados em arquivo

--- Cursos/Topicos_Rodney/Praticas/BrokenAcess/main.py
+++ Cursos/Topicos_Rodney/Praticas/BrokenAcess/main.py
@@ -1,15 +1,22 @@
 import sqlite3
 
 conn = sqlite3.connect("users.db")
-cursos = conn.cursor()
+cursor = conn.cursor()
 
-# Criar a Tabela de Usúarios
-cursos.execute("""
-CREATE TABLE IS NOT EXISTS users(
-    id INTENGER PRIMARY KEY AUTOINCREMENT,
+# Criar a Tabela de Usuários
+cursor.execute("""
+CREATE TABLE IF NOT EXISTS users (
+    id INTEGER PRIMARY KEY AUTOINCREMENT,
     name TEXT NOT NULL,
-    password TEXT NOT NULL             
+    password TEXT NOT NULL
 );
 """)
 
+
+# Inserir um usuário teste
+cursor.execute("INSERT INTO users(name, password) VALUES ('juanAdmin', '1234')")
+conn.commit()
+conn.close()
+
+print('Banco de Dados Criado com Sucesso!')
 
