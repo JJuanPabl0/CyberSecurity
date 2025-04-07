ec438776b626a9e785e307e4ce96e2fcd0af2206
JJuanPabl0 <juansilverio865@gmail.com>
2025-04-07 11:39:44 -0300

    feat: Criação da tabela de usuarios, e, cria o banco de dados se não existir

--- Cursos/Topicos_Rodney/Praticas/BrokenAcess/main.py
+++ Cursos/Topicos_Rodney/Praticas/BrokenAcess/main.py
@@ -4,3 +4,12 @@ conn = sqlite3.connect("users.db")
 cursos = conn.cursor()
 
 # Criar a Tabela de Usúarios
+cursos.execute("""
+CREATE TABLE IS NOT EXISTS users(
+    id INTENGER PRIMARY KEY AUTOINCREMENT,
+    name TEXT NOT NULL,
+    password TEXT NOT NULL             
+);
+""")
+
+
